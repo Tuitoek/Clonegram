c79bbc4b6e12636630f0f86a13f8e2c81b41e392
Sarah Tuitoek <sjtoek@gmail.com>
2019-03-11 09:59:26 +0300

    Updated settings.html

--- instagram/settings.py
+++ instagram/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'bootstrap3'
+    'bootstrap3',
     'gram.apps.GramConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -120,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
