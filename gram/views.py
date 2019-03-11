f957d52ee6f1034fe0e5b229ca1b00debeb3298b
Sarah Tuitoek <sjtoek@gmail.com>
2019-03-11 09:59:06 +0300

    Add views.py

--- gram/views.py
+++ gram/views.py
@@ -4,3 +4,12 @@ from django.http import HttpResponse
 # Create your views here.
 def landing(request):
     return render(request,'landing.html')
+
+def home(request):
+    return render(request,'home.html')
+
+def profile(request):
+    return render(request,'profile.html')
+
+def home2(request):
+    return render(request,'home2.html')    
