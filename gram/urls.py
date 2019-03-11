71d274f8da62fe462f67d347aa7bb400e9ee2ea5
Sarah Tuitoek <sjtoek@gmail.com>
2019-03-11 09:58:57 +0300

    Add urls.py

--- gram/urls.py
+++ gram/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns=[
     url('^$',views.landing,name = 'landing'),
+    url('^home/',views.home2,name = 'home'),
+    url('^profile/',views.profile,name = 'profile')
 ]
