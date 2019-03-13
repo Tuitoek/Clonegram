a69c39ceabad48b9acf5e9b139eb054ad8d59430
Sarah Tuitoek <sjtoek@gmail.com>
2019-03-13 13:15:31 +0300

    Updated urls.py

--- gram/urls.py
+++ gram/urls.py
@@ -1,19 +1,26 @@
-from django.conf.urls import url,include
+from django.conf.urls import url, include
 from django.contrib.auth import views as auth_views
 from . import views as core_views
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
 
-urlpatterns=[
-    url('^$',views.landing,name = 'landing'),
-    url('^home/',views.home2,name = 'home'),
-    url('^profile/',views.profile,name = 'profile'),
-    url('^search/',views.searchresults,name='searchresults'),
-    url('^register/',views.register,name='register'),
+
+urlpatterns = [
+    url('^$', views.landing, name='landing'),
+    url('^home/', views.home2, name='home'),
+    url('^profile/', views.profile, name='profile'),
+    url('^search/', views.searchresults, name='searchresults'),
+    url('^register/', views.register, name='register'),
     url(r'^accounts/', include('registration.backends.simple.urls')),
-    url('^accounts/login/$',auth_views.LoginView.as_view(template_name='registration/login.html'),name='login'),
-    url(r'^logout/$',auth_views.LogoutView.as_view(template_name='registration/logout.html'),name='logout'),
+    url('^accounts/login/$', auth_views.LoginView.as_view(
+        template_name='registration/login.html'), name='login'),
+    url(r'^logout/$', auth_views.LogoutView.as_view(
+        template_name='registration/logout.html'), name='logout'),
+    url(r'^comments/', views.comments, name="comments"),
+    url(r'^admin/$', views.test_redirect, name='test_redirect'),
+    url('^profile/editprofile/',    views.profile_form, name='profile_form'),
+    url(r'^friendship/', include('friendship.urls')),
 
 ]
 if settings.DEBUG:
