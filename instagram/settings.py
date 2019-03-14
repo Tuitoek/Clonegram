a5fb62b62d4a97b68f13a5886d8d09cf4829d8e2
Sarah Tuitoek <sjtoek@gmail.com>
2019-03-14 09:33:33 +0300

    deployment take 13

--- instagram/settings.py
+++ instagram/settings.py
@@ -110,6 +110,16 @@ WSGI_APPLICATION = 'instagram.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME':'gram',
+        'USER':'postgres',
+        'PASSWORD': 'sarahjt12',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
 
 
 # Password validation
